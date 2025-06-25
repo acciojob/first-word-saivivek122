666f75e90e06731d3790bdb81fe869a7cbd61991
saivivek122 <119473212+saivivek122@users.noreply.github.com>
2025-06-25 06:58:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 function firstWord(s) {
   // your code here
-}
+	s=s.trim();
+	if(s.length==0){
+        return '';
+	}
 
+	let spaceIndex=s.indexOf(" ");
+	if(spaceIndex==-1){
+         return s;
+	}
+	else{
+
+	   return s.substring(0,spaceIndex);
+	}
+}
+   
 // Do not change the code below
 
 const s = prompt("Enter String:");
